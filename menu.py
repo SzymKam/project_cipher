2e4e6e40f1556a4153663cc41c73e710bce252f4
SzymKam <szymon15kaminski@gmail.com>
2022-10-18 17:09:53 +0200

    feat: add menu file

---
+++ menu.py
@@ -0,0 +1,12 @@
+
+class Menu:
+    @staticmethod
+    def print_main_menu():
+        print("Welcome to main menu of Cezar cipher program\n "
+              "Choose an option:\n"
+              "1. Encode / decode \n"
+              "2. Check buffer \n"
+              "3. Load text from file \n"
+              "4. Save buffer to file \n"
+              "5. Exit program")
+
