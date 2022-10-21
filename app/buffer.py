d81903adfa9315b5eaf3b1f721d9213e0e78929a
SzymKam <szymon15kaminski@gmail.com>
2022-10-21 13:16:37 +0200

    feat: update buffer file, new features

--- app/buffer.py
+++ app/buffer.py
@@ -1,44 +1,50 @@
 class Buffer:
+    program_buffer = []
+
+    @staticmethod
+    def add_to_buffer(new_element):
+        Buffer.program_buffer.append(new_element)
+
+    @staticmethod
+    def clear_buffer():
+        Buffer.program_buffer.clear()
+        print("Buffer empty!\n")
+
+    @staticmethod
+    def show_buffer():
+        for counter, element in enumerate(Buffer.program_buffer):
+            print(f"Element number: {counter + 1} \n"
+                  f"Text: {element.text} \n"
+                  f"Status: {element.status} \n"
+                  f"Rot type: {element.rot_type} \n")
+
+
+class BufferManager:
     def __init__(self):
-        # self.choice = None
-        self.__buffer = []
+        self.__choice = None
 
-    def add_to_buffer(self, new_element):
-        self.__buffer.append(new_element)
+    def buffer_run(self):
+        self.buffer_menu()
+        self.user_choice()
+        self.program_choice(choice=self.__choice)
 
-    def clear_buffer(self):
-        self.__buffer.clear()
+    @staticmethod
+    def buffer_menu():
+        print("BUFFER MENU: \n"
+              "Check buffer - 1\n"
+              "Clear buffer - 2\n")
 
-    def show_buffer(self):
-        for element in self.__buffer:
-            print(element)
+    def user_choice(self):
+        self.__choice = input("What you want to do? \n")
 
-    # @staticmethod
-    # def buffer_menu():
-    #     print("You are in buffer.\n"
-    #           "If you want check buffer enter: 1\n"
-    #           "If you want clear buffer enter: 2\n")
-    #
-    # def user_choice(self):
-    #     self.choice = int(input("What you want to do? \n"))
-    #
-    # def program_choice(self, choice: int) -> None:
-    #     if choice == 1:
-    #         if self.__buffer:
-    #             self.show_buffer()
-    #         else:
-    #             print("Buffer is empty!\n")
-    #     elif choice == 2:
-    #         self.clear_buffer()
-    #         print("Buffer clear!\n")
-    #     else:
-    #         raise ValueError
-    #
-    # def run_buffer(self):
-    #     self.buffer_menu()
-    #     self.user_choice()
-    #     self.program_choice(choice=self.choice)
-
-#
-# my_buffer = Buffer()
-
+    @staticmethod
+    def program_choice(choice: str):
+        if choice == "1":
+            if Buffer.program_buffer:
+                Buffer.show_buffer()
+            else:
+                print("Buffer is empty!\n")
+        elif choice == "2":
+            Buffer.clear_buffer()
+        else:
+            raise ValueError
