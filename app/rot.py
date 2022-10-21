f5d63c913197ed0408dc9f9b8b9397f131163d1a
SzymKam <szymon15kaminski@gmail.com>
2022-10-21 13:19:32 +0200

    feat: update rot file, change code for rot_manager

--- app/rot.py
+++ app/rot.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import string
-import buffer
+from buffer import Buffer
+from text import Text
 
 
 class Rot(ABC):
@@ -23,8 +24,9 @@ class Rot47(Rot):
         self._type: str = "rot" + str(self._shift)
         self._encoded = ""
         self._decoded = ""
+        self._rot_status = ""
 
-    def encode(self, text_to_encode: str) -> str:
+    def encode(self, text_to_encode: str) -> list:
         text_to_encode = text_to_encode.lower()
         for char in text_to_encode:
             if char in string.ascii_lowercase:
@@ -32,9 +34,10 @@ class Rot47(Rot):
                 if new_char > 122:
                     new_char = 96 + (new_char - 122)
             self._encoded += chr(new_char)
-        return self._encoded
+            self._rot_status = "encoded"
+        return [self._encoded, self._rot_status]
 
-    def decode(self, text_to_decode: str):
+    def decode(self, text_to_decode: str) -> list:
         text_to_decode = text_to_decode.lower()
         for char in text_to_decode:
             if char in string.ascii_lowercase:
@@ -42,7 +45,8 @@ class Rot47(Rot):
                 if new_char < 97:
                     new_char = 123 - (97 - new_char)
             self._decoded += chr(new_char)
-        return self._decoded
+            self._rot_status = "decoded"
+        return [self._decoded, self._rot_status]
 
     def rot_type(self) -> str:
         return f"{self._type}"
@@ -54,8 +58,9 @@ class Rot13(Rot):
         self._type: str = "rot" + str(self._shift)
         self._encoded = ""
         self._decoded = ""
+        self._rot_status = ""
 
-    def encode(self, text_to_encode: str) -> str:
+    def encode(self, text_to_encode: str) -> list:
         text_to_encode = text_to_encode.lower()
         for char in text_to_encode:
             if char in string.ascii_lowercase:
@@ -63,9 +68,10 @@ class Rot13(Rot):
                 if new_char > 122:
                     new_char = 96 + (new_char - 122)
             self._encoded += chr(new_char)
-        return self._encoded
+            self._rot_status = "encoded"
+        return [self._encoded, self._rot_status]
 
-    def decode(self, text_to_decode: str):
+    def decode(self, text_to_decode: str) -> list:
         text_to_decode = text_to_decode.lower()
         for char in text_to_decode:
             if char in string.ascii_lowercase:
@@ -73,7 +79,8 @@ class Rot13(Rot):
                 if new_char < 97:
                     new_char = 123 - (97 - new_char)
             self._decoded += chr(new_char)
-        return self._decoded
+            self._rot_status = "decoded"
+        return [self._decoded, self._rot_status]
 
     def rot_type(self) -> str:
         return f"{self._type}"
@@ -91,46 +98,48 @@ class RotFactory:
 class RotManager(RotFactory):
 
     def __init__(self) -> None:
-        self.result = None
-        self.done = None
-        self.__selected_method = None
-        self.__choose = None
-        self.__working_rot = None
-        self.__text: str = ""
-        # self.my_buffer = buffer.Buffer
+        self.__result = None
+        self.__code = None
+        self.__option = None
+        self.__text = ""
 
-    def start(self) -> None:
-        self.menu()
-        self.choose_cipher()
-        self.input_text()
-        self.__working_rot = self.get_rot(shift=self.__choose)
-        self.choose_method()
-        self.result = self.encrypt(method=self.__selected_method)
-        buffer.my_buffer.add_to_buffer(self.result)
+    def rot_run(self) -> None:
+        self.rot_menu()
+        self.__option = input("Enter number of rot option: \n")
+        self.rot_working(self.__option)
+        Buffer.add_to_buffer(self.text_return())
+        print("Added to buffer!\n")
 
     @staticmethod
-    def menu() -> None:
-        print("You are in rot menu\n"
-              "Choose rod type:\n"
-              "For use Cezar cipher 13 - enter 13\n"
-              "For use Cezar cipher 47 - enter 47\n")
+    def rot_menu() -> None:
+        print("ROT MENU: \n"
+              "For encrypt text in Rot13 enter - 1 \n"
+              "For decrypt text in Rot13 enter - 2 \n"
+              "For encrypt text in Rot47 enter - 3 \n"
+              "For decrypt text in Rot47 enter - 4 \n")
 
-    def choose_cipher(self) -> None:
-        self.__choose = input("What type of cipher do you want? \n")
-        print(f"You choose Cipher{self.__choose}\n")
+    def rot_working(self, decision: str):
+        if decision in ["1", "2", "3", "4"]:
+            print(f"You choose option {self.__option}")
+            self.__text = input("Enter text: \n")
+            self.make_coding(decision=decision)
+        else:
+            print("Invalid Value")
 
-    def input_text(self) -> None:
-        self.__text = input("Enter text: \n")
+    def make_coding(self, decision: str):
+        if decision == "1" or decision == "2":
+            self.__code = self.get_rot(shift="13")
+            if decision == "1":
+                self.__result = (self.__code.encode(self.__text))
+            elif decision == "2":
+                self.__result = self.__code.decode(self.__text)
+        elif decision == "3" or decision == "4":
+            self.__code = self.get_rot(shift="47")
+            if decision == "3":
+                self.__result = self.__code.encode(self.__text)
+            elif decision == "4":
+                self.__result = self.__code.decode(self.__text)
+        print("Task done")
 
-    def choose_method(self) -> None:
-        print("If you want encode enter: 1 \n"
-              "If you want decode enter: 2 \n")
-        self.__selected_method = int(input("Do you want do decode or encode? \n"))
-
-    def encrypt(self, method: int) -> dict:
-        if method == 1:
-            self.done = self.__working_rot.encode(text_to_encode=self.__text)
-            return {"cipher": self.__working_rot.rot_type(), "text": self.done, "status": "encrypted"}
-        elif method == 2:
-            self.done = self.__working_rot.decode(text_to_decode=self.__text)
-            return {"cipher": self.__working_rot.rot_type(), "text": self.done, "status": "decrypted"}
+    def text_return(self) -> Text:
+        return Text(text=self.__result[0], status=self.__result[1], rot_type=self.__code.rot_type())
