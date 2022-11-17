83ef0e322250c1bf51376e8e3383884d501ca83b
SzymKam <szymon15kaminski@gmail.com>
2022-11-17 23:46:14 +0100

    feat: update test manager

--- app/tests/test_manager.py
+++ app/tests/test_manager.py
@@ -1,3 +1,19 @@
+from manager import Manager
+import pytest
 
 
+class TestManager:
 
+    @pytest.fixture
+    def setup_tests(self):
+        self.test_manager = Manager()
+
+    def test_if_handle_instruction_print_invalid_option_with_choice_out_of_option(self, setup_tests, capsys):
+        self.test_manager.handle_instruction(choice=7)
+        captured = capsys.readouterr()
+        assert captured.out == 'Invalid option.\n'
+
+    def test_if_manager_end_program(self, setup_tests, capsys):
+        self.test_manager._Manager__end_app()
+        captured = capsys.readouterr()
+        assert captured.out == 'Program shutting down.\n'
