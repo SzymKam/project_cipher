240556b714bc157ee136ecb044dce161349c4284
SzymKam <szymon15kaminski@gmail.com>
2022-11-17 13:31:20 +0100

    feat: update text

--- app/text.py
+++ app/text.py
@@ -1,10 +1,8 @@
-class Text:
-    def __init__(self, text, status, rot_type):
+class TextElements:
+    def __init__(self, text, status, rot_type) -> None:
         self.text = text
         self.status = status
         self.rot_type = rot_type
 
-
-
-
-
+    def return_text(self) -> str:
+        return self.text
