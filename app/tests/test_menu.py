e8e28312e945ef67b7d1fadac29429748012fe9c
SzymKam <szymon15kaminski@gmail.com>
2022-11-17 23:45:13 +0100

    feat: update test menu

--- app/tests/test_menu.py
+++ app/tests/test_menu.py
@@ -1,9 +1,13 @@
 from menu import Menu
+MAIN_MENU = ("MAIN MENU: \n"
+             "1. Encode / decode \n"
+             "2. Buffer options \n"
+             "3. Filehandler options\n"
+             "4. Exit program\n"
+             "\n")
 
 
-def test_if_menu_print_correctly():
-    assert Menu.print_main_menu() == print("MAIN MENU: \n"
-                                           "1. Encode / decode \n"
-                                           "2. Buffer options \n"
-                                           "3. Filehandler options\n"
-                                           "4. Exit program\n")
+def test_if_menu_print_correctly(capsys):
+    Menu.print_main_menu()
+    capture = capsys.readouterr()
+    assert capture.out == MAIN_MENU
