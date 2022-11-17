c002d54f74ee2c89f8f921963c67ed974e4e8453
SzymKam <szymon15kaminski@gmail.com>
2022-11-18 00:04:36 +0100

    feat: update test filehandler

---
+++ app/tests/test_filehandler.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+import buffer
+from file_handler import FilehandlerManager
+from text import TextElements
+import temporary_dir
+
+
+class TestFilehandler:
+    @pytest.fixture
+    def setup_test(self):
+        self.test_filehandler_manager = FilehandlerManager()
+
+    # def teardown_method(self):
+    #     os.rmdir(app.temporary_dir.test_file.json)
+
+    def test_if_make_decision_print_invalid_option_with_choice_out_of_range(self, setup_test):
+        assert self.test_filehandler_manager.make_decision(choice='some_option') == print("Invalid option")
+
+    def test_if_save_return_right_value(self, mocker, setup_test):
+        mocker.patch(buffer.Buffer, 'program_buffer', TextElements(text="test_text",
+                                                                   status="tet_status",
+                                                                   rot_type="test_rot_type"))
+        self.test_filehandler_manager.save(filename='test_file', save_option='w', directory='temporary_dir')
+        with not pytest.raises(FileNotFoundError):
+            assert open(temporary_dir.test_file.json, 'rb').read() == {'a': 'a'}
