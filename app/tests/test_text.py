46542b92065c4d82ee8ca70f9812f170ba48ddf3
SzymKam <szymon15kaminski@gmail.com>
2022-11-17 21:50:44 +0100

    feat: update test text file

--- app/tests/test_text.py
+++ app/tests/test_text.py
@@ -5,4 +5,3 @@ class TestText:
     def test_if_return_text_return_right_value(self):
         test_text_elements = TextElements(text='test_text', status='test_status', rot_type='test_rot_type')
         assert test_text_elements.return_text() == 'test_text'
-
